57b52e58280379bc12104a832471c25f38d86d6c
Le Hong Duc <hongducthbk123@gmail.com>
2025-03-30 18:05:27 +0700

    Initial MiuiCamera for beryllium
    Co-authored-by: Arian <arian.kulmer@web.de>

---
+++ BoardConfigCamera.mk
@@ -0,0 +1,16 @@
+#
+# SPDX-FileCopyrightText: 2025 The Evolution X Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/xiaomi/miuicamera-beryllium/BoardConfigVendor.mk
+
+MIUICAMERA_PATH := device/xiaomi/miuicamera-beryllium
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(MIUICAMERA_PATH)/product.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
---
+++ config.mk
@@ -0,0 +1,24 @@
+#
+# SPDX-FileCopyrightText: 2025 The Evolution X Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/miuicamera-beryllium/miuicamera-beryllium-vendor.mk)
+
+# Config
+PRODUCT_COPY_FILES += \
+    device/xiaomi/miuicamera-beryllium/config/default-permissions/miuicamera-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/miuicamera-permissions.xml \
+    device/xiaomi/miuicamera-beryllium/config/device_features/beryllium.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/beryllium.xml \
+    device/xiaomi/miuicamera-beryllium/config/permissions/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    device/xiaomi/miuicamera-beryllium/config/permissions/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.front.xml \
+    device/xiaomi/miuicamera-beryllium/config/permissions/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.full.xml \
+    device/xiaomi/miuicamera-beryllium/config/permissions/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.raw.xml \
+    device/xiaomi/miuicamera-beryllium/config/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml \
+    device/xiaomi/miuicamera-beryllium/config/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt \
+
+# SysConfig
+PRODUCT_COPY_FILES += \
+    device/xiaomi/miuicamera-beryllium/config/sysconfig/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml \
+
+include device/xiaomi/miuicamera-beryllium/BoardConfigCamera.mk
